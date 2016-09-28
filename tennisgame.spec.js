f8f3431d86bbb4b81fadf7b706311da25fd63054
57160042 <57160042@go.buu.ac.th>
2016-09-29 03:46:55 +0700

    Test case

--- tennisgame.spec.js
+++ tennisgame.spec.js
@@ -75,7 +75,7 @@ this.echo = function(){
         }
 
         else if( ScoreA === 30 && ScoreB === 30){
-                return 'Forty - Forty';
+                return 'Thirty - Thirty';
         }
 
         else if( ScoreA === 55 ){
@@ -92,17 +92,17 @@ describe("Tennis game", function() {
     expect(tennis.echo()).toEqual('Love - Love');
   });
 
-  it('should be 0-15 "Love - Fifteen" when starting the game', function() {
+  it('should be 0-15 "Love - Fifteen" when A=0,B=15', function() {
     tennis.PlayerGetScoreB();
     expect(tennis.echo()).toEqual('Love - Fifteen');
   });
 
-  it('should be 0-30 "Love - Thirty" when starting the game', function() {
+  it('should be 0-30 "Love - Thirty" when A=0,B=30', function() {
     tennis.PlayerGetScoreB();
     expect(tennis.echo()).toEqual('Love - Thirty');
   });
 
-  it('should be 0-40 "Love - Forty" when starting the game', function() {
+  it('should be 0-40 "Love - Forty" when A=0,B=40', function() {
     tennis.PlayerGetScoreB();
     expect(tennis.echo()).toEqual('Love - Forty');
   });
@@ -112,25 +112,38 @@ describe("Tennis game", function() {
     expect(tennis.echo()).toEqual('playerB Win');
   });
 
-  it('should be 15-0 "Fifteen - Love" when starting the game', function() {
+  it('should be 15-0 "Fifteen - Love" when A=15,B=0', function() {
     tennis.start();
     tennis.PlayerGetScoreA();
     expect(tennis.echo()).toEqual('Fifteen - Love');
   });
 
-  it('should be 30-0 "Thirty - Love" when starting the game', function() {
+  it('should be 30-0 "Thirty - Love" when A=30,B=0', function() {
     tennis.PlayerGetScoreA();
     expect(tennis.echo()).toEqual('Thirty - Love');
   });
 
-  it('should be 40-0 "Forty - Love" when starting the game', function() {
+  it('should be 40-0 "Forty - Love" when A=40,B=0', function() {
     tennis.PlayerGetScoreA();
     expect(tennis.echo()).toEqual('Forty - Love');
   });
 
-  it('should be  "playerA Win" when playerB Win', function() {
+  it('should be  "playerA Win" when playerA Win', function() {
     tennis.PlayerGetScoreA();
     expect(tennis.echo()).toEqual('playerA Win');
+  });
+
+  it('should be 15-15 "Fifteen - Fifteen" when A=15,B=15', function() {
+    tennis.start();
+    tennis.PlayerGetScoreA();
+    tennis.PlayerGetScoreB();
+    expect(tennis.echo()).toEqual('Fifteen - Fifteen');
+  });
+
+  it('should be 30-30 "Thirty - Thirty" when A=30,B=30', function() {
+    tennis.PlayerGetScoreA();
+    tennis.PlayerGetScoreB();
+    expect(tennis.echo()).toEqual('Thirty - Thirty');
   });
 
 });
