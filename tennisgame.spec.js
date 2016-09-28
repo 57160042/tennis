d15f5daf707fcceced16d0345577388431dd7a12
Thunyaporn <thunthunyaporn@gmail.com>
2016-09-29 03:35:23 +0700

    Edit

--- tennisgame.spec.js
+++ tennisgame.spec.js
@@ -55,7 +55,7 @@ this.echo = function(){
         }
 
         else if( ScoreB === 55 ){
-               return 'PlayerB Win';
+               return 'playerB Win';
         }
 
        else if( ScoreA === 15 && ScoreB === 0){
@@ -92,24 +92,24 @@ describe("Tennis game", function() {
     expect(tennis.echo()).toEqual('Love - Love');
   });
 
-  it('should be 15-0 "Fifteen - Love" when starting the game', function() {
-    tennis.PlayerGetScoreA();
-    expect(tennis.echo()).toEqual('Fifteen - Love');
+  it('should be 0-15 "Love - Fifteen" when starting the game', function() {
+    tennis.PlayerGetScoreB();
+    expect(tennis.echo()).toEqual('Love - Fifteen');
   });
 
-  it('should be 30-0 "Thirty - Love" when starting the game', function() {
-    tennis.PlayerGetScoreA();
-    expect(tennis.echo()).toEqual('Thirty - Love');
+  it('should be 0-30 "Love - Thirty" when starting the game', function() {
+    tennis.PlayerGetScoreB();
+    expect(tennis.echo()).toEqual('Love - Thirty');
   });
 
-  it('should be 40-0 "Forty - Love" when starting the game', function() {
-    tennis.PlayerGetScoreA();
-    expect(tennis.echo()).toEqual('Forty - Love');
+  it('should be 0-40 "Love - Forty" when starting the game', function() {
+    tennis.PlayerGetScoreB();
+    expect(tennis.echo()).toEqual('Love - Forty');
   });
 
-  it('should be  "playerA Win" when playerA Win', function() {
-    tennis.PlayerGetScoreA();
-    expect(tennis.echo()).toEqual('playerA Win');
+  it('should be  "playerB Win" when playerB Win', function() {
+    tennis.PlayerGetScoreB();
+    expect(tennis.echo()).toEqual('playerB Win');
   });
 
   it('should be 15-0 "Fifteen - Love" when starting the game', function() {
@@ -128,9 +128,9 @@ describe("Tennis game", function() {
     expect(tennis.echo()).toEqual('Forty - Love');
   });
 
-  it('should be  "playerB Win" when playerB Win', function() {
-    tennis.PlayerGetScoreB();
-    expect(tennis.echo()).toEqual('playerB Win');
+  it('should be  "playerA Win" when playerB Win', function() {
+    tennis.PlayerGetScoreA();
+    expect(tennis.echo()).toEqual('playerA Win');
   });
 
 });
